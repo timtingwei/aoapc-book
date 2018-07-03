7916f589e461db82d2786f144f9f181814b030cc
timtingwei <timtingwei@hotmail.com>
2018-07-03 12:54:18 +0800

    add decimal

---
+++ BeginningAlgorithmContests/exercises/ch2/ex2-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  int a, b, c, kase = 1;
+  while ((scanf("%d%d%d", &a, &b, &c) == 3) && a && b && c) {
+    printf("Case %d: %d.", kase++, a/b);
+    int i = 1;
+    while (i < c) {
+      a *= 10;
+      printf("%d", a / b);
+      a %= b;
+      i++;
+    }
+    a *= 10;
+    printf("%d\n", ((a % b * 10 / b >= 5) ? (a / b + 1) : (a / b)));
+  }
+
+  return 0;
+}
