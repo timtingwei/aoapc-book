c48c46c7088978c76212d0bd3942a0ae4509eaf3
timtingwei <timtingwei@hotmail.com>
2018-07-04 18:14:33 +0800

    add snake

--- BeginningAlgorithmContests/bookscodes/ch3/3-3.c
+++ BeginningAlgorithmContests/bookscodes/ch3/3-3.c
@@ -2,38 +2,34 @@
 // snake
 
 #include <stdio.h>
-#define MAXN 100
-int a[MAXN];
-int a_drc[MAXN];
+#include <string.h>
+#include <time.h>
+#define MAXN 100000
+#define MAXM 100000
+int a[MAXM][MAXN];
 
 int main() {
-  int n;
+  int n, x, y, tot = 0;
   scanf("%d", &n);
-  int drc = 2, drc_cnt = n -1, cnt = 0;
-  for (int i = 0; i < 2*n-1; i++) {
-    for (int j = 0; j < drc_cnt; j++) {
-      a_drc[cnt++] = drc;
-    }
-    if ((i != 0) && (i % 2 == 0)) drc_cnt--;
-    drc = (++drc == 4) ? 0 : drc;
+  memset(a, 0, sizeof(a));
+
+  tot = a[x = 0][y = n-1] = 1;
+  while (tot < n * n) {
+    while (x + 1 <  n && !a[x+1][y]) a[++x][y] = ++tot;
+    while (y - 1 >= 0 && !a[x][y-1]) a[x][--y] = ++tot;
+    while (x - 1 >= 0 && !a[x-1][y]) a[--x][y] = ++tot;
+    while (y + 1 <  n && !a[x][y+1]) a[x][++y] = ++tot;
   }
 
-  int x = 1, y = n;
-  a[y] = 1;
-  for (int i = 0; i < cnt; i++) {
-    if (a_drc[i] == 0) x--;  // 0 : x--;
-    if (a_drc[i] == 1) y++;  // 1 : y++;
-    if (a_drc[i] == 2) x++;  // 2 : x++;
-    if (a_drc[i] == 3) y--;  // 3 : y--;
-    a[(x-1)*n + y] = i+2;
-    // printf("%d %d\n", (x-1)*n + y, i+2);
-  }
-
-  cnt = 0;
   int first = 1;
-  for (int i = 1; i <= n*n; i++) {
-    if (first) first = 0; else printf(" "); printf("%d ", a[i]);
-    if (++cnt == n) {cnt = 0; first = 1; printf("\n");}
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (first) first = 0; else printf(" "); printf("%d", a[i][j]);
+    }
+    printf("\n");
+    first = 1;
   }
+
+  printf("Use time: %f\n", (double)clock()/CLOCKS_PER_SEC);
   return 0;
 }
