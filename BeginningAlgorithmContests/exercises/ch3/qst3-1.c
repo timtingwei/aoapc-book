330134ae4649d83641f773f39e6577d4667e658c
timtingwei <timtingwei@hotmail.com>
2018-07-17 15:26:30 +0800

    add question 3-1-4~6

--- BeginningAlgorithmContests/exercises/ch3/qst3-1.c
+++ BeginningAlgorithmContests/exercises/ch3/qst3-1.c
@@ -34,11 +34,11 @@ int foo3() {
   // 输入一些数, 哪两个数最接近  => 用数组
   int a[105], b, n = 0;
   while (scanf("%d", &b) == 1) { a[n++] = b; }
-  
+
   int min_pos = 1, min = MAX_INT;
   for (int i = 1; i < n; i++) {
     int mi = (a[i-1] < a[i]) ? a[i] - a[i-1] : a[i-1] - a[i];
-    printf("mi = %d\n", mi);
+
     if (mi < min) { min = mi; min_pos = i;}
   }
   printf("%d %d\n", a[min_pos - 1], a[min_pos]);
@@ -47,10 +47,59 @@ int foo3() {
   return 0;
 }
 
+int foo4() {
+  // 输入一些数, 求第二大的值  => 不用数组
+  int a, max1 = MIN_INT, max2;
+  while (scanf("%d", &a) == 1) {
+    // if (max1 < a) { max2 = max1; max1 = a;}
+    if (max2 < a) {
+      if (a < max1) {
+        max2 = a;
+      } else {
+        max2 = max1; max1 = a;
+      }
+    }
+  }
+  printf("%d\n", max2);
+
+  return 0;
+}
+
+int foo5() {
+  // 输入一些数, 求他们的方差  => 用数组
+  int a[105], b, n = 0, sum = 0;
+  while (scanf("%d", &b) == 1) {a[n++] = b; sum += b;}
+  
+  float s = 0.0, aver = sum / (float)n;
+  for (int i = 0; i < n; i++) {
+    s += (a[i] - aver) * (a[i] - aver);
+  }
+  s /= n;
+  printf("%f\n", s);
+  return 0;
+}
+
+int foo6() {
+  // 输入一些数, 统计不超过平均数的个数 => 用数组
+  int a[105], b, n = 0, sum = 0;
+  float aveg;
+  while (scanf("%d", &b) == 1) { a[n++] = b; sum += b;}
+  aveg = sum / (float)n;
+
+  int cnt = 0;
+  for (int i = 0; i < n; i++) { if (a[i] < aveg) cnt++; }
+  printf("%d\n", cnt);
+
+  return 0;
+}
+
 int main() {
   // foo1();
   // foo2();
-  foo3();
+  // foo3();
+  // foo4();
+  // foo5();
+  foo6();
 
   return 0;
 }
