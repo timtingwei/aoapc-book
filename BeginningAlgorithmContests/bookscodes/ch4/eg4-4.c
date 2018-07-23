cd2e56ab98641cea403405655a696660986081ff
timtingwei <timtingwei@hotmail.com>
2018-07-23 22:04:25 +0800

    debug io representation UVa 213 Message Decoding(信息解码)

--- BeginningAlgorithmContests/bookscodes/ch4/eg4-4.c
+++ BeginningAlgorithmContests/bookscodes/ch4/eg4-4.c
@@ -1,11 +1,21 @@
 // copyright [2018] <mituh>
 // UVa 213 Message Decoding(信息解码)
 /*
-S#**\
+Sample input
+TNM AEIOU
+0010101100011
+1010001001110110011
+11000
+$#**\
 0100000101101100011100101000
+Sample output
+TAN ME ##
+*\$*(\)
+
 */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #define MAXN 100    // 字符串长度
 #define MAXM 100   // 编码文本长度
 #define MAXD 150    // 解码字符串长度
@@ -16,12 +26,12 @@ char ans_s[MAXN];     // 输出
 int N;
 
 
-
 void generate_s3(int n) {
   // 生成奇怪的二进制字符串
   int b = 1, cnt = 0;
-  while (cnt <= n) {
-    for (int i = 0; i < ((1 << b)-1); i++) {  // ^比-的优先级低 ^是位异运算符
+  for (;;) {
+    int bad = 0;
+    for (int i = 0; (i < ((1 << b)-1)); i++) {  // ^比-的优先级低 ^是位异运算符
       char st[10] = {};
       memset(st, '0', sizeof(char) * b);
       int j = b-1, k = i;
@@ -32,10 +42,14 @@ void generate_s3(int n) {
       } while (k);
       for (int x = 0; x < b; x++) { s3[cnt][x] = st[x];}
       cnt++;
+      if (cnt == n) {bad = 1; break;}
     }
-    b++;  // 改变位数
+    if (bad) break;
+    b++;  // 改变位数(阶段)
   }
-  // for (int i = 0; i < cnt; i++) printf("%s\n", s3[i]);
+  // printf("cnt  = %d, n = %d\n", cnt, n);
+  // printf("    s3 = \n");
+  // for (int i = 0; i < cnt; i++) printf("      %s\n", s3[i]);
 }
 
 int btoi(int* a, int n) {
@@ -70,6 +84,7 @@ int find_in_s(int* a, int n) {
   }
   if (all_one) {return -1;}
   int find = compare(s3, N, find_s);   // 大小为N的s3中, 找find_s索引, 没找到-1
+  // printf("find = %d\n", find);
   /*
   for (int i = 0; i < n; i++)
     if (find_s == s3[i]) {find = i; break;}   // 不同字符串之间, 不能单纯的比较
@@ -96,7 +111,7 @@ int go(int* p, int t, int st) {
   return ans;
 }
 
-// /*
+/*
 int main() {
 #ifdef LOCAL
   freopen("a.in", "r", stdin);
@@ -124,17 +139,102 @@ int main() {
   }
   return 0;
 }
-// */
+*/
 
-/*
-int main() {
-  // test compare
-  generate_s3(10);
-  N = 10;
-  char mys[20] = "00";
-  int p = compare(s3, 10, mys);
-  printf("%d\n", p);
+
+int decode() {
+  // 解码当前的s1, s2
+  // 每次解码前清空s1, s2, s3数组, 以及答案
+  // memset(s1, '\0', sizeof(s1));
+  // memset(s2, '\0', sizeof(s2));
+  memset(s3, '\0', sizeof(s3));
+  // memset(ans_s, '\0', sizeof(ans_s));
+  int n = strlen(s2);
+  N = strlen(s1);   // 传递到全局变量
+  generate_s3(N);  // 生成s3
+  int cnt = 0;
+  int p = 0, t = 3, st = 0;   // 当前位置, 步长, 状态:先找编码长度
+  while (p < n) {
+    if (!st) {  // 找出编码长度
+      t = go(&p, t, 0);
+      st = 1;
+    } else {   // s=1, 寻找对应长度的解码字母, 以及结束字母
+      int si = 0;
+      si = go(&p, t, 1);
+      if (si != -1) {  // 不是结束字符
+        // printf("si = %d\n", si);
+        ans_s[cnt++] = s1[si];
+      } else { t = 3; st = 0;}
+    }
+  }
+  printf("%s\n", ans_s);
 
   return 0;
 }
+/*
+s1 = $ #  *  *  \
+     0 00 01 10 000
+     0 1  2  3  4
+s2 = 010 00 00 10 11  011 000 111 001 0 1  000
+     (2) 1  1  3  \n  (3) 4   \n  (1) 0 \n
 */
+
+
+int main() {
+#ifdef LOCAL
+  freopen("a.in", "r", stdin);
+#endif
+  /*
+  整体用getchar读入所有字符, 每次读入一个c时,
+  if (st= 0) 时,
+    if (读到'\n') {s2[n2++] = c, st -> 1;}
+    else s1[n1++] = c;
+  if (st = 1) 时
+    if (读到除了'0', '1', '\n'以外的字符) {
+      st -> 0; 进行一次解码; 清空数组;
+      if (c == EOF) {break;退出循环}
+    }
+    else s2[n2++] = c;
+  */
+  int c = 0, n1 = 0, n2 = 0, st = 0;
+  memset(s1, '\0', sizeof(s1));
+  memset(s2, '\0', sizeof(s2));
+  for (;;) {
+    c = getchar();
+    if (st == 0) {
+      if (c == '\n') {
+        st = 1;
+      } else {  // 代码里, c是除了'\n'以外的字符, 实际上我要求的是字母字符
+        // printf("s1[%d] = %c\n", n1, c);
+        s1[n1++] = c;
+      }
+    } else if (st == 1) {
+      if (c == '0' || c == '1' || c == '\n') {
+        if (c == '0' || c == '1') {
+          // printf("s2[%d] = %c\n", n2, c);
+          s2[n2++] = c;
+        }
+      } else {  // 读s2的时候, 读到0,1,\n以外的字符
+        st = 0;
+        // printf("s1 = %s\n  s2 = %s\n", s1, s2);
+        decode();
+        memset(s1, '\0', sizeof(s1));
+        memset(s2, '\0', sizeof(s2));
+        memset(ans_s, '\0', sizeof(ans_s));
+        n1 = 0, n2 = 0;
+        // printf("add s1[%d] = %c\n", n1, c);
+        s1[n1++] = c;
+        if (c == EOF) { break;}
+      }
+    }
+  }
+  return 0;
+}
+
+
+int foo() {
+  int n = 4;
+  generate_s3(n);
+
+  return 0;
+}
